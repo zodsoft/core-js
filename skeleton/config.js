5ea153d60b11054efe24240e05f1c855be6fcc4c
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-06 14:26:43 -0400

    Cleaned up config

--- skeleton/config.js
+++ skeleton/config.js
@@ -1,54 +1,38 @@
 
-/* General Configuration*/
-
-var config = {
+module.exports = {
+  
   title: 'My Application',
   language: 'en-US',
   encoding: 'utf-8',
-  rawViews: false
-};
-
-
-/* Regular Expressions */
-
-config['regex'] = {};
-
-
-/* Custom & Dynamic Headers */
-
-config['headers'] = {
-  'Content-Type': function(req, res) { return "text/html; charset=" + this.config.encoding; },
-  'Date': function() { return new Date().toUTCString(); },
-  'Status': function(req, res) {  return res.statusCode + " " + this.httpStatusCodes[res.statusCode]; },
-  'X-Powered-By': 'CoreJS/' + framework.version
-};
-
-
-/* Server Settings */
-
-config['server'] = {
-  strictRouting: true,
-  headRedirect: false,
-  maxFieldSize: 2 * 1024 * 1024,
-  maxUploadSize: 2 * 1024 * 1024,
-  keepUploadExtensions: true,
-};
-
-
-/* Static Server Settings */
-
-config['staticServer'] = {
-  eTags: true,
-  acceptRanges: true,
-};
-
-/* Cache Control Settings */
-
-config['cacheControl'] = {
-  maxAge: 10 * 365 * 24 * 60 * 60,
-  static: 'public',
-  dynamic: 'private, must-revalidate, max-age=0',
-  error: 'no-cache'
-};
-
-module.exports = config;
+  rawViews: false,
+  
+  regex: {},
+  
+  headers: {
+    'Content-Type': function(req, res) { return "text/html; charset=" + this.config.encoding; },
+    'Date': function() { return new Date().toUTCString(); },
+    'Status': function(req, res) {  return res.statusCode + " " + this.httpStatusCodes[res.statusCode]; },
+    'X-Powered-By': 'core-js/' + framework.version
+  },
+  
+  server: {
+    strictRouting: true,
+    headRedirect: false,
+    maxFieldSize: 2 * 1024 * 1024,
+    maxUploadSize: 2 * 1024 * 1024,
+    keepUploadExtensions: true
+  },
+  
+  staticServer: {
+    eTags: true,
+    acceptRanges: true
+  },
+  
+  cacheControl: {
+    maxAge: 10 * 365 * 24 * 60 * 60,
+    static: 'public',
+    dynamic: 'private, must-revalidate, max-age=0',
+    error: 'no-cache'
+  }
+  
+}
