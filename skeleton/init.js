5fc44fa462eb1aceeb29a03a4ce99e685b45fd06
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-06 18:35:13 -0400

    Implemented redis storage

--- skeleton/init.js
+++ skeleton/init.js
@@ -1,15 +1,6 @@
 
 function Initialize(app) {
 
-  var storage = new framework.storage.redis(app, {host: 'localhost', port: 6379, db: 1});
-
-  storage.set('hello', 'world', function(err) {
-    if (err) throw err
-    else {
-      console.log('successfully set value');
-    }
-  });
-
 }
 
 module.exports = Initialize;
--- storage/redis.js
+++ storage/redis.js
@@ -1,19 +1,13 @@
 
-/* RedisStorage 
-
-   options:
-   
-    {
-      host: 'localhost',
-      port: 6379,
-      db: 1,
-      pass: 'password'
-
-    }
-
-*/
+/* RedisStorage */
 
 function RedisStorage(app, config) {
+  
+  /** { host: 'localhost',
+        port: 6379,
+        db: 1,
+        pass: 'password'
+      } */
   
   this.constructor.prototype.__construct.call(this, app, config);
   
@@ -23,7 +17,8 @@ function RedisStorage(app, config) {
 
 framework.extend(RedisStorage.prototype, new function() {
   
-  var redis = require('redis');
+  var redis = require('redis'),
+      util = require('util');
 
   this.options = {
     // Parser defaults to hiredis if installed
@@ -62,16 +57,116 @@ framework.extend(RedisStorage.prototype, new function() {
     framework.util.onlySetEnumerable(this, ['className', 'config']);
   }
   
+
+  /** Storage API get */
+  
   this.get = function(key, callback) {
+    var self = this;
     
+    // If key is a string
+    if (typeof key == 'string') {
+      
+      this.client.get(key, function(err, data) {
+        if (err) callback.call(self, err, null);
+        else {
+          callback.call(self, null, data);
+        }
+      })
+      
+    // If key is an array
+    } else if (util.isArray(key)) {
+      
+      var i, out = {}, keys = key, 
+          multi = this.client.multi();
+          
+      for (i=0; i < keys.length; i++) {
+        multi.get(keys[i]);
+      }
+      
+      multi.exec(function(err, data) {
+        if (err) callback.call(self, err);
+        else {
+          for (i=0; i < keys.length; i++) {
+            out[keys[i]] = data[i];
+          }
+          callback.call(self, null, out);
+        }
+      })
+    }
   }
+  
+  
+  /** Storage API set */
   
   this.set = function(key, value, callback) {
+    var self = this;
     
+    // If key is a string
+    if (typeof key == 'string') {
+      
+      // Set single value
+      this.client.set(key, value, function(err, data) {
+        if (err) callback.call(self, err);
+        else {
+          callback.call(self, null);
+        }
+      });
+      
+    // If key is an array
+    } else if (typeof key == 'object') {
+      
+      // Set multiple values
+      var object = key,
+          callback = value,
+          multi = this.client.multi();
+          
+      for (key in object) {
+        multi.set(key, object[key]);
+      }
+      
+      multi.exec(function(err, data) {
+        if (err) callback.call(self, err);
+        else {
+          callback.call(self, null);
+        }
+      });
+      
+    }
   }
   
+  
+  /** Storage API delete */
+  
   this.delete = function(key, callback) {
+    var self = this;
     
+    // If key is a string
+    if (typeof key == 'string') {
+      
+      this.client.del(key, function(err, data) {
+        if (err) callback.call(self, err);
+        else {
+          callback.call(self, null);
+        }
+      });
+      
+    // If key is an array
+    } else if (util.isArray(key)) {
+      var i, keys = key,
+          multi = this.client.multi();
+          
+      for (i=0; i < keys.length; i++) {
+        multi.del(keys[i]);
+      }
+      
+      multi.exec(function(err, data) {
+        if (err) callback.call(self, err);
+        else {
+          callback.call(self, null);
+        }
+      });
+      
+    }
   }
   
 });
