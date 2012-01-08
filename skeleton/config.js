255011fe80418b2b2885d66ac090fe232511d5f8
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-08 00:11:50 -0400

    Redefined framework's enumerable properties

--- skeleton/config.js
+++ skeleton/config.js
@@ -12,7 +12,8 @@ module.exports = {
     'Content-Type': function(req, res) { return "text/html; charset=" + this.config.encoding; },
     'Date': function() { return new Date().toUTCString(); },
     'Status': function(req, res) {  return res.statusCode + " " + this.httpStatusCodes[res.statusCode]; },
-    'X-Powered-By': 'core-js/' + framework.version
+    'X-Powered-By': 'core-js/' + framework.version,
+    'X-Node-Version': process.version.replace(/^v/,'')
   },
   
   server: {
