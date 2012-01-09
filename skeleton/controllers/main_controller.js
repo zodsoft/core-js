daaefe4db7fef0547fa63356c5c2a89f2936e63e
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-08 21:36:18 -0400

    update

--- skeleton/controllers/main_controller.js
+++ skeleton/controllers/main_controller.js
@@ -1,9 +1,32 @@
 
 function MainController() {
 
+  this.authRequired = false;
+  
   get('/', function(req, res) {
     res.render('index');
-  })
+  });
+  
+  get('/login', function(req, res) {
+    res.rawHttpMessage('Please <a href="/auth">authenticate</a>');
+  });
+  
+  get('/auth', function(req ,res) {
+   if (req.__session.user) app.home(res);
+   else {
+     app.session.create(req, res, {user: 'ernie'}, true, function(session) {
+       app.home('res');
+     });
+   }
+  });
+  
+  get('/logout', function(req, res) {
+    if (req.__session.user) {
+      app.session.destroy(function() {
+        app.home(res);
+      });
+    } else app.home(res);
+  });
 
 }
 
