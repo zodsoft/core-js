cb1712b8cca24f719d70d8e983790c3a06c5c41e
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-11 19:13:29 -0400

    Cleanup

--- skeleton/config/storage.js
+++ skeleton/config/storage.js
@@ -4,21 +4,8 @@
 module.exports = {
 
   redis: {
-    queryCache: {
-      host: 'localhost',
-      port: 6379,
-      db: 0
-    },
-    sessionStore: {
-      host: 'localhost',
-      port: 6379,
-      db: 1
-    },
-    responseCache: {
-      host: 'localhost',
-      port: 6379,
-      db: 2
-    }
+    host: 'localhost',
+    port: 6379
   }
 
 }
