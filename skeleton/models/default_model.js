86c400c55b6224318cf0c9c334fa98281effc6d2
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-10 21:07:45 -0400

    Working on model api

--- skeleton/models/default_model.js
+++ skeleton/models/default_model.js
@@ -1,6 +1,23 @@
 
 function DefaultModel(app) {
 
+  // Property Definitions
+
+  this.name = '';
+  this.email = '';
+  this.limit = 3;
+  this.hello = true;
+  this.stuff = null;
+
+  // Relationships
+  
+  this.hasOne('friend');
+  this.hasMany('colleagues');
+
+  // Methods
+  
+  
+
 }
 
 module.exports = DefaultModel;
