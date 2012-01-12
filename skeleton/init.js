42feb6f0558ad9a5e446d99bdb1a5492115a284a
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-11 23:36:37 -0400

    Driver cacheStorage now working

--- skeleton/init.js
+++ skeleton/init.js
@@ -1,17 +1,6 @@
 
 function Initialize(app) {
 
-  app.debugMode = true;
-
-  app.drivers.mysql.queryById({
-    id: 1,
-    table: 'users',
-    cacheID: 'my_cache',
-    cacheTimeout: 3000
-  }, function(err, results) {
-    console.log(results);
-  });
-
 }
 
 module.exports = Initialize;
