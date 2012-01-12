cb96d3f80a312d028cfe2aa6ea31bb368cc48315
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-12 16:20:37 -0400

    Updated users model

--- skeleton/models/users_model.js
+++ skeleton/models/users_model.js
@@ -2,8 +2,8 @@
 function UsersModel(app) {
   
   this.properties = {
-    id    : {type: 'integer', unique: true},
-    user  : {type: 'string', required: true, validates: 'alpha'},
+    id    : {type: 'integer'},
+    user  : {type: 'string', unique: true, required: true, validates: 'alpha'},
     pass  : {type: 'string', required: true, validates: 'password'},
     date  : {type: 'timestamp', validates: 'timestamp'}
   }
