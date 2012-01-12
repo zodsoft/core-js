5c01d307f7c44c8762c04e285f8d07990931eb9b
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-12 18:54:49 -0400

    Multi now saves any existing `exec` methods into `__exec`.

--- skeleton/init.js
+++ skeleton/init.js
@@ -1,6 +1,11 @@
 
 function Initialize(app) {
 
+  var driver = app.getResource("drivers/mysql"),
+      multi = driver.multi();
+  
+  console.exit(multi.exec.toString());
+
   app.debugMode = true;
 
   app.usersModel.new({
@@ -8,7 +13,7 @@ function Initialize(app) {
     pass: 'abcd1234',
   }, {
     cacheID: 'mycache',
-    cacheTimeout: 60 * 1000
+    cacheTimeout: 60
   }, function(err, user) {
     console.exit([err, user]);
   });
