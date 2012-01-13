4e54326401f24899ad640d7828c6944c6977ecdb
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-13 12:14:47 -0400

    Added aliases to ModelObject

--- skeleton/init.js
+++ skeleton/init.js
@@ -3,26 +3,6 @@ function Initialize(app) {
 
   app.debugMode = true;
   
-  // var multi = app.usersModel.multi();
-  // multi.insert({user: 'ernie', pass: 'abc123'});
-  // multi.insert({user: 'howdy', pass: 'def456'});
-  // multi.insert({user: 'heya' , pass: 'ghi789'});
-  // multi.exec(function(err, results) {
-  //   console.exit([err, results]);
-  // });
-  
-  // app.usersModel.get([85, {user: 'howdy'}, 87], function(err, models) {
-  //   console.exit([err, models]);
-  // });
-  
-  // app.usersModel.delete([85, 86, 87], function(err, results) {
-  //   console.exit([err, results]);
-  // });
-
-  // app.usersModel.getAll(function(err, models) {
-  //   console.exit([err, models]);
-  // });
-  
 }
 
 module.exports = Initialize;
