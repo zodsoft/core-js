7641fb3f4e9e59c7277d4aca7df916aa390e18f9
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-14 16:08:53 -0400

    Added command line colors for linting tool

--- tools/lint.js
+++ tools/lint.js
@@ -6,7 +6,7 @@
 
 var path = require('path'),
     cp = require('child_process');
-    
+
 // Framework path
 var rootPath = path.dirname(__filename);
 
@@ -17,10 +17,22 @@ var config = path.resolve(rootPath, '../jshint.json');
 var cmd = path.resolve(rootPath, '../node_modules/jshint/bin/hint'),
     args = process.argv.slice(2).concat(['--config', config]);
 
+// Output colors
+var colorRegex = /(\n)?(.*?): line (\d+), (.*?), (.*?)(\n)/g
+
+// Functions
+function logger(data) {
+  data = data.toString('utf-8');
+  console.log(colorize(data));
+}
+
+function colorize(data) {
+  return data.replace(colorRegex, '$1➜ \033[0;30m$2:\033[0m\033[1;30m$3 \033[0;31m$5\033[0m$6');
+}
+
 // Run command w/args
 console.log('');
-var jshint = cp.spawn(cmd, args),
-    logger = function(data) { console.log(data.toString('utf-8')); };
+var jshint = cp.spawn(cmd, args);
 
 jshint.stdout
   .on('data', logger)
